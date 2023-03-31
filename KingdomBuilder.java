ded04be771b166d64ad0957635cbc08d0d5c5618
PermutatedData <89464108+PermutatedData@users.noreply.github.com>
2023-03-31 13:43:53 -0500

    Preliminary work here

--- KingdomBuilder.java
+++ KingdomBuilder.java
@@ -1,18 +1,45 @@
-import java.awt.*;
-import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.CardLayout;
 
 public class KingdomBuilder extends JFrame {
-    private static final int WIDTH = 1600;
-    private static final int HEIGHT = 960;
+    public static final int WIDTH = 1600;
+    public static final int HEIGHT = 960;
+
+    private KingdomBuilderPanel gamePanel;
+    private CardLayout cardLayout;
     
-    public KingdomBuilder(String title) {
-        super(title); 
+    public KingdomBuilder() {
+        super("Kingdom Builder"); 
+
+        cardLayout = new CardLayout();
+        setLayout(cardLayout);
+
+        add(new StartPanel(this));
+        gamePanel = new KingdomBuilderPanel(this);
+        add(gamePanel);
+        add(new EndPanel(this));
+
+        pack();
+
+        cardLayout.first(getContentPane());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH, HEIGHT);       
-        add(new KingdomBuilderPanel());
+        setResizable(false);
         setVisible(true);
-        
     }
     
-    
+    public void nextScreen() {
+        cardLayout.next(getContentPane());
+    }
+
+    public void showBoard() {
+
+    }
+
+    public void restart() {
+
+    }
+
+    public static void main(String[] args) {
+        new KingdomBuilder();
+    }   
 }
