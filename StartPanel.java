31b4a91a29c2344deb4e7669e1f002c22eefb413
K1206505 <89477040+K1206505@users.noreply.github.com>
2023-04-03 13:41:40 -0500

    startscreen changes

--- StartPanel.java
+++ StartPanel.java
@@ -4,11 +4,14 @@ import javax.swing.JButton;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class StartPanel extends JPanel implements ActionListener {
     private KingdomBuilder frame;
 
     private JButton startButton;
+    private BufferedImage startImage;
 
     public StartPanel(KingdomBuilder kb) {
         setLayout(null);
@@ -21,10 +24,17 @@ public class StartPanel extends JPanel implements ActionListener {
         startButton.setFocusable(false);
         startButton.addActionListener(this);
         add(startButton);
+        try {
+            startImage = ImageIO.read(StartPanel.class.getResource("/Images/start.png"));
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception Error");
+        }
+
     }
     
     public void paintComponent(Graphics g) {
-        
+        g.drawImage(startImage, KingdomBuilder.WIDTH, KingdomBuilder.HEIGHT, null);
     }
 
     public void actionPerformed(ActionEvent e) {
