63d8caa31454880ad74da3e9824eaa0f8289fb04
K1206505 <89477040+K1206505@users.noreply.github.com>
2023-04-05 13:21:48 -0500

    blurred bg in + wood bg in kevin

--- KingdomBuilderPanel.java
+++ KingdomBuilderPanel.java
@@ -1,17 +1,28 @@
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class KingdomBuilderPanel extends JPanel implements ActionListener, MouseListener {
     private KingdomBuilder frame;
+    private BufferedImage blurBG,wood;
 
     public KingdomBuilderPanel(KingdomBuilder kb) {
        setSize(getPreferredSize());
        setLayout(null);
        frame = kb;
+       try {
+            blurBG = ImageIO.read(KingdomBuilderPanel.class.getResource("/Images/blurred BG.jpg"));
+            wood = ImageIO.read(KingdomBuilderPanel.class.getResource("/Images/wood.png"));
+       } catch (Exception e) {
+        // TODO: handle exception
+        System.out.println("Exception Error");
+       }
 
        addMouseListener(this);
     }
@@ -20,6 +31,8 @@ public class KingdomBuilderPanel extends JPanel implements ActionListener, Mouse
         Graphics2D g = (Graphics2D)l;
         
         g.clearRect(0, 0, getWidth(), getHeight());
+        g.drawImage(blurBG, 0, 0, getWidth(), getHeight(), null);
+        g.drawImage(wood, 1100, 60, 450, 840, null);
     }
 
     public void mousePressed(MouseEvent e) {}
