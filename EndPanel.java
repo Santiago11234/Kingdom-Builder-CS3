b7de41fdf86c422a9b6b6fdc635b090044cdc33f
PermutatedData <89464108+PermutatedData@users.noreply.github.com>
2023-05-02 13:42:57 -0500

    Beginning ranks

--- EndPanel.java
+++ EndPanel.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
 import java.util.HashMap;
 
 import javax.swing.JPanel;
@@ -16,6 +17,9 @@ public class EndPanel extends JPanel implements ActionListener{
 
     private String[] objectives;
     private HashMap<String, ArrayList<Integer>> scores;
+    private TreeSet<Integer>[] playerRanks;
+
+    private int[] totalScores;
 
     public EndPanel(KingdomBuilder kb) {
         setSize(getPreferredSize());
@@ -53,6 +57,41 @@ public class EndPanel extends JPanel implements ActionListener{
     public void setObjectivesAndScore(String[] obj, HashMap<String, ArrayList<Integer>> scores) {
         objectives = obj;
         this.scores = scores;
+
+        totalScores = new int[4];
+
+        ArrayList<Integer> temp;
+        for(String objective: scores.keySet()) {
+            temp = scores.get(objective);
+            
+            for(int i = 0; i < temp.size(); i++) {
+                totalScores[i] += temp.get(i);
+            }
+        }
+
+        rankPlayers();
+    }
+
+    private void rankPlayers() {
+        int numPlayersRanked = 0;
+        int rankAt = 0;
+
+        while(numPlayersRanked < 4) {
+
+        }
+        playerRanks[rankAt]= new TreeSet<Integer>();
+        int maxScoreIndex = 0;
+        for(int i = 1; i < totalScores.length; i++) {
+            if(totalScores[i] > totalScores[maxScoreIndex]) {
+                maxScoreIndex = i;
+            }
+        }
+
+        for(int i = 0; i < totalScores.length; i++) {
+            if(totalScores[i] == totalScores[maxScoreIndex]) {
+                playerRanks[rankAt].add(i);
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
